919b3971a84753e9cc6af588ff396fd2ec990212
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-09-22 12:43:06 +0530

    Add : Method to display current level on screen

--- scoreboard.py
+++ scoreboard.py
@@ -1,6 +1,7 @@
 from turtle import Turtle
 
 FONT = ("Courier", 20, "bold")
+ALIGNMENT = "right"
 
 
 class Scoreboard(Turtle):
@@ -10,9 +11,9 @@ class Scoreboard(Turtle):
         self.level = 1
         self.hideturtle()
         self.penup()
-        self.set_playground()
+        self.update_scoreboard()
 
-    def set_playground(self):
+    def set_playground(self) -> None:
         """
         Draw start and finish lines on our screen
         """
@@ -21,12 +22,20 @@ class Scoreboard(Turtle):
         self.goto(-280, -180)
         self.pendown()
         self.goto(260, -180)
-        self.write("Start", align="right", font=FONT)
+        self.write("Start", align=ALIGNMENT, font=FONT)
 
         # Draw finishing line
         self.penup()
         self.goto(-280, 200)
         self.pendown()
         self.goto(260, 200)
-        self.write("Finish", align="right", font=FONT)
+        self.write("Finish", align=ALIGNMENT, font=FONT)
+        self.penup()
 
+    def update_scoreboard(self) -> None:
+        """
+        Update scoreboard
+        """
+        self.set_playground()
+        self.goto(-150, 250)
+        self.write(f"Level: {self.level}", align=ALIGNMENT, font=FONT)
