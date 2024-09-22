6dd83c1f3906a2c1503be78f679cefbae97036dd
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-09-22 12:58:49 +0530

    Add : Logic to check if player has reached finish line and bring back player to starting position

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 from turtle import Turtle
 
-STARTING_POSITION = (0, -200)
+STARTING_POSITION = (0, -220)
 MOVE_DISTANCE = 10
 FINISH_LINE = 200
 
@@ -16,8 +16,20 @@ class Player (Turtle):
         self.goto(STARTING_POSITION)
         self.setheading(90)
 
-    def move(self):
+    def move(self) -> None:
         """
         Move turtle
         """
         self.forward(MOVE_DISTANCE)
+
+    def reached_finish_line(self) -> bool:
+        """
+        Returns true if player has crossed the finish line
+        """
+        return self.ycor() >= FINISH_LINE
+
+    def back_to_starting_position(self) -> None:
+        """
+        Bring player back to starting line
+        """
+        self.goto(STARTING_POSITION)
