9cfc17868ff24855f0869ef8dd9a653700f97e30
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-09-25 19:17:23 +0530

    Add : Logic to store high score in data file

--- scoreboard.py
+++ scoreboard.py
@@ -17,7 +17,7 @@ class ScoreBoard(Turtle):
         self.pencolor("white")
         self.goto(SCOREBOARD_X, SCOREBOARD_Y)
         self.score = -1
-        self.high_score = 0
+        self.high_score = self.read_high_score()
         self.update_scoreboard()
 
     def update_scoreboard(self) -> None:
@@ -36,6 +36,25 @@ class ScoreBoard(Turtle):
         """
         if self.score > self.high_score:
             self.high_score = self.score
+            self.write_high_score(self.high_score)
 
         self.score = -1
         self.update_scoreboard()
+
+    @staticmethod
+    def read_high_score() -> int:
+        """
+        Read high score from data file and return it
+        """
+        with open(file="data.txt", mode="r") as file:
+            high_score = file.read()
+            high_score = int(high_score)
+        return high_score
+
+    @staticmethod
+    def write_high_score(high_score: int) -> None:
+        """
+        Write new high score in data file
+        """
+        with open(file="data.txt", mode="w") as file:
+            file.write(str(high_score))
