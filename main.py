fc47c9bc7d903f45ba2de8f55a459f9111ebf14b
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-10-06 15:44:14 +0530

    Add : Logic to flip the card and show meaning of kanji

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 # Flash Card Learning app
+from idlelib.configdialog import font_sample_text
 from tkinter import *
 import pandas as pd
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+KANJI_FONT = ("Arial", 75, "bold")
+MEANING_FONT = ("Arial", 30, "bold")
 
 # --------------------------- APP DATA SETUP -----------------------------------#
 kanji_data = pd.read_csv("data/kanji.csv")
@@ -18,9 +21,27 @@ def next_card() -> None:
     random_kanji = random.choice(to_learn)
 
     # Update canvas
-    canvas.itemconfig(kanji, text=random_kanji.get("Kanji"))
+    canvas.itemconfig(canvas_image, image=card_front)
+    canvas.itemconfig(title, text="Japanese Kanji")
+    canvas.itemconfig(kanji, text=random_kanji.get("Kanji"), font=KANJI_FONT)
+    canvas.itemconfig(english_meaning, text="")
     canvas.itemconfig(onyomi, text=f"Onyomi: {random_kanji.get("Onyomi")}")
     canvas.itemconfig(kunyomi, text=f"Kunyomi: {random_kanji.get("Kunyomi")}")
+
+    # Flip card after 3 seconds
+    window.after(3000, flip_card, random_kanji)
+
+
+def flip_card(kanji_card: dict):
+    """
+    This function flips the card which is the answer to the question
+    """
+    canvas.itemconfig(canvas_image, image=card_back)
+    canvas.itemconfig(title, text="English Meaning")
+    canvas.itemconfig(english_meaning, text=f"{kanji_card["Kanji Meaning"]}")
+    canvas.itemconfig(kanji, text="")
+    canvas.itemconfig(onyomi, text="")
+    canvas.itemconfig(kunyomi, text="")
 
 
 # --------------------------- UI SETUP -----------------------------------#
@@ -32,12 +53,15 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 # Add card image using canvas
 canvas = Canvas(width=800, height=526)
 card_front = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_front)
+card_back = PhotoImage(file="images/card_back.png")
+
+canvas_image = canvas.create_image(400, 263, image=card_front)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 
 # Add kanji texts on top of our canvas
 title = canvas.create_text(400, 100, text="Japanese Kanji", font=("Arial", 25, "italic"))
 kanji = canvas.create_text(400, 200, text="&", font=("Arial", 75, "bold"))
+english_meaning = canvas.create_text(400, 250, text="", font=MEANING_FONT, width=500)
 onyomi = canvas.create_text(400, 320, text="Onyomi:", font=("Arial", 20, "italic"), width=600)
 kunyomi = canvas.create_text(400, 380, text="Kunyomi:", font=("Arial", 20, "italic"), width=600)
 
